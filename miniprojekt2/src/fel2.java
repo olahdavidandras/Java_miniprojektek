dbad521d8acb0c3df2e6f6dde714cd4a1c35d86c
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-02-28 09:12:51 +0200

    Update fel2.java

--- miniprojekt2/src/fel2.java
+++ miniprojekt2/src/fel2.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class fel2 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kerek egy szot");
+        String szo = scanner.nextLine();
+        String csillagszo="";
+        for(int i=0;i<szo.length();i++){
+            if(szo.charAt(i)=='a' || szo.charAt(i)=='A'){
+                csillagszo+="*";
+            }else {
+                csillagszo+=szo.charAt(i);
+            }
+        }
+        System.out.println(csillagszo);
+
+        scanner.close();
     }
 }
