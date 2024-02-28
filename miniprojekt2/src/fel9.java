6a407a81b19717d6b27e97b45c7c19c2cbe578ea
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-02-28 14:40:15 +0200

    Create fel9.java

---
+++ miniprojekt2/src/fel9.java
@@ -0,0 +1,21 @@
+public class fel9 {
+    public static void main(String[] args) {
+        int[][] matrix= new int[10][10];
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<10;j++){
+                if (i==j){
+                    matrix[i][j]=i;
+                }else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i=0;i<10;i++) {
+            for (int j = 0; j < 10; j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.println();
+        }
+
+    }
+}
