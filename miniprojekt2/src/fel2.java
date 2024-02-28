3379ac0b194389c4301f7fbd1ccca0b1aeebfd4b
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-02-28 15:34:17 +0200

    Valtoztatas

--- miniprojekt2/src/fel2.java
+++ miniprojekt2/src/fel2.java
@@ -5,12 +5,12 @@ public class fel2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kerek egy szot");
         String szo = scanner.nextLine();
-        String csillagszo="";
-        for(int i=0;i<szo.length();i++){
-            if(szo.charAt(i)=='a' || szo.charAt(i)=='A'){
-                csillagszo+="*";
-            }else {
-                csillagszo+=szo.charAt(i);
+        String csillagszo = "";
+        for (int i = 0; i < szo.length(); i++) {
+            if (szo.charAt(i) == 'a' || szo.charAt(i) == 'A') {
+                csillagszo += "*";
+            } else {
+                csillagszo += szo.charAt(i);
             }
         }
         System.out.println(csillagszo);
---
+++ miniprojekt2/src/fel5.java
@@ -0,0 +1,21 @@
+public class fel5 {
+    public static void main(String[] args) {
+        String[] tomb = {"alma", "korte", "banan", "szolo", "avocado", "cipo"};
+        if (tomb.length != 0) {
+            for (int i = 0; i < tomb.length - 1; i++) {
+                tomb[i] = tomb[i + 1];
+            }
+            tomb[tomb.length - 1] = null;
+        }
+        if (tomb.length > 1) {
+            tomb[tomb.length - 2] = null;
+
+        }
+        for (String elem : tomb) {
+            if (elem != null) {
+                System.out.println(elem);
+            }
+
+        }
+    }
+}
