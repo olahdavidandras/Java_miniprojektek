405202376cc113a8f8dcf0a0664dacded300286d
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-03-21 09:01:29 +0200

    2.

--- miniprojekt3/src/projekt/Film.java
+++ miniprojekt3/src/projekt/Film.java
@@ -11,11 +11,12 @@ interface IKorhataros {
 public class Film {
     protected String cim;
     protected int ar;
-    protected ArrayList<Integer> velemenyek = new ArrayList<>(20);
+    protected int[] velemenyek;
 
     public Film(String cim, int ar) {
         this.cim = cim;
         this.ar = ar;
+        this.velemenyek = new int[20];
     }
 
     public String getCim() {
@@ -31,22 +32,36 @@ public class Film {
     }
 
     public void velemenyezes(int velemeny) {
-        if (velemeny >= 1 && velemeny <= 5) {
-            velemenyek.add(velemeny);
-        } else {
-            throw new RuntimeException("1-5 kozott kell legyen az ertekeles!");
+        if (velemeny < 1 || velemeny > 5) {
+            throw new RuntimeException("1-5 között kell legyen az értékelés!");
+        }
+        boolean helyTalalva = false;
+        for (int i = 0; i < 20; i++) {
+            if (velemenyek[i] == 0) {
+                velemenyek[i] = velemeny;
+                helyTalalva = true;
+                break;
+            }
+        }
+        if (!helyTalalva) {
+            throw new RuntimeException("A vélemények tömb tele van!");
         }
     }
 
+
     public double atlag() {
         double ossz = 0;
+        double oszto = 0;
         for (int i : velemenyek) {
             ossz += i;
+            if (i != 0) {
+                oszto += 1;
+            }
         }
         if (ossz == 0) {
             throw new RuntimeException("Meg nem kapott ertekelest!");
         }
-        return ossz / velemenyek.size();
+        return ossz / oszto;
     }
 
     @Override
--- miniprojekt3/src/projekt/Teszter.java
+++ miniprojekt3/src/projekt/Teszter.java
@@ -3,6 +3,18 @@ package projekt;
 import java.util.ArrayList;
 
 public class Teszter {
+
+    public static double buntetes(int eletkor, ArrayList<Film> lista) {
+        double buntetesOsszes = 0;
+        for (Film film : lista) {
+            if (film instanceof IKorhataros) {
+                IKorhataros korhatosFilm = (IKorhataros) film;
+                buntetesOsszes += korhatosFilm.buntetes(eletkor);
+            }
+        }
+        return buntetesOsszes;
+    }
+
     public static void main(String[] args) {
         Film f1 = new Film("Halaslos Iramban X", 12);
         Film f2 = new Film("Az", 20);
@@ -11,14 +23,14 @@ public class Teszter {
         Film f5 = new Film("Keresztapa", 16);
         Film f6 = new Film("Taxi IV", 21);
 
-        HorrorFilm hf1= new HorrorFilm("Furesz",10,16);
-        HorrorFilm hf2= new HorrorFilm("Fekete telefon",14,16);
-        HorrorFilm hf3= new HorrorFilm("Az apaca 2.",21,18);
-        HorrorFilm hf4= new HorrorFilm("Frankenstein",17,16);
-        HorrorFilm hf5= new HorrorFilm("Demonok kozott",25,18);
-        HorrorFilm hf6= new HorrorFilm("Sikoly VI",15,16);
-        ArrayList<Film> lista= new ArrayList<>();
-        ArrayList<IKorhataros> lista2= new ArrayList<>();
+        HorrorFilm hf1 = new HorrorFilm("Furesz", 10, 16);
+        HorrorFilm hf2 = new HorrorFilm("Fekete telefon", 14, 16);
+        HorrorFilm hf3 = new HorrorFilm("Az apaca 2.", 21, 18);
+        HorrorFilm hf4 = new HorrorFilm("Frankenstein", 17, 16);
+        HorrorFilm hf5 = new HorrorFilm("Demonok kozott", 25, 18);
+        HorrorFilm hf6 = new HorrorFilm("Sikoly VI", 15, 16);
+        ArrayList<Film> lista = new ArrayList<>();
+        ArrayList<IKorhataros> lista2 = new ArrayList<>();
 
         lista.add(f1);
         lista.add(f2);
@@ -33,7 +45,7 @@ public class Teszter {
         lista.add(hf5);
         lista.add(hf6);
 
-        for (Film film:lista){
+        for (Film film : lista) {
             System.out.println(film);
         }
         System.out.println();
@@ -48,20 +60,25 @@ public class Teszter {
             }
         }
 
-        for (Film film:lista){
+        for (Film film : lista) {
             System.out.println(film);
         }
         System.out.println();
 
-        for (Film film:lista){
-            if (film instanceof HorrorFilm){
+        for (Film film : lista) {
+            if (film instanceof HorrorFilm) {
                 lista2.add((HorrorFilm) film);
             }
         }
-        for (IKorhataros  film:lista2){
+        for (IKorhataros film : lista2) {
             System.out.println(film);
         }
         System.out.println();
+
+        System.out.println("Buntetes 18 evesre: " + buntetes(18, lista));
+        System.out.println("Buntetes 17 evesre: " + buntetes(17, lista));
+        System.out.println("Buntetes 16 evesre: " + buntetes(16, lista));
+        System.out.println("Buntetes 15 evesre: " + buntetes(15, lista));
 
     }
 }
